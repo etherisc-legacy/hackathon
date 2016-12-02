1d374604ca25d46ce4c042113aedda690df89a78
Jake Brukhman <jbrukh@gmail.com>
2016-12-02 10:23:44 -0500

    Fixed options

--- etherisc-simulator/bin/riscsim.py
+++ etherisc-simulator/bin/riscsim.py
@@ -4,21 +4,21 @@
 # @Author: Jake Brukhman
 # @Date:   2016-11-30 20:43:17
 # @Last Modified by:   Jake Brukhman
-# @Last Modified time: 2016-12-01 17:18:38
+# @Last Modified time: 2016-12-02 10:22:33
 
 """
 riscsim.py
 Etherisc decentralized insurance model simulator.
 
 Usage:
-  riscsim.py estimaterandom [-n N] [-p PAYOUT]
-  riscsim.py estimatedata FILENAME [-p PAYOUT] [-r SAMPLESIZE]
+  riscsim.py estimaterandom [-n=<N>] [-p=<PAYOUT>]
+  riscsim.py estimatedata FILENAME [-p=<PAYOUT>] [-r=<SAMPLESIZE>]
 
 
 Options:
-  -n, --events N          the number of insurable events [default: 10]
-  -p, --payout PAYOUT     the average payout parameter [default: 500]
-  -r, --random            select a random sample of events     
+  -n, --events=<N>              the number of insurable events [default: 10]
+  -p, --payout=<PAYOUT>         the average payout parameter [default: 500]
+  -r, --random=<SAMPLESIZE>     select a random sample of events [default: 0] 
 """
 
 from docopt import docopt
@@ -32,6 +32,7 @@ def main(args):
   """
   n             = int(args['--events'])
   payout        = int(args['--payout'])
+  samplesize    = int(args['--random'])
 
   if args['estimaterandom']:
     """
@@ -43,9 +44,6 @@ def main(args):
     """
     Estimate the Etherisc credit model from a CSV file.
     """
-    samplesize = 0         # zero means don't do a random sample
-    if args['--random']:
-      samplesize = int(args['SAMPLESIZE'])
     estimatedata(args['FILENAME'], payout=payout, randomsample=samplesize)
 
   elif args['flightdata']:
@@ -64,5 +62,6 @@ if __name__ == '__main__':
   Read the command line arguments.
   """
   args = docopt(__doc__)
+  print(args)
   settings()
   main(args)
