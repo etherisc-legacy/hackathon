357a3dfb54be85481e364d1fc0faefe56fdc1894
Jake Brukhman <jbrukh@gmail.com>
2016-12-03 16:19:22 -0500

    Factored out data loading function.

--- etherisc-simulator/etherisc/output.py
+++ etherisc-simulator/etherisc/output.py
@@ -2,7 +2,7 @@
 # @Author: Jake Brukhman
 # @Date:   2016-11-30 22:01:55
 # @Last Modified by:   Jake Brukhman
-# @Last Modified time: 2016-12-02 10:27:29
+# @Last Modified time: 2016-12-03 16:19:05
 
 from scipy.stats import norm, uniform
 from etherisc.variable import VariableEstimator
@@ -22,15 +22,10 @@ def estimatedata(filename, datatype='flightcsv', payout=500, randomsample=0, min
   """
   Load actual data for model estimation.
   """
-
-  # process the data
-  if datatype is 'flightcsv':
-    data = extract_flight_csv(filename, minprob=minprob, maxprob=maxprob)
-    if randomsample > 0:
-      data = data.sample(randomsample)
-  else:
-    raise Exception('unknown datatype %s' % datatype)
-
+  data = loaddata(filename, datatype='flightcsv')
+  if randomsample > 0:
+    data = data.sample(randomsample)
+  
   # get the model parameters
   probs = data['probs']
   payouts = [payout] * len(probs)
@@ -39,3 +34,13 @@ def estimatedata(filename, datatype='flightcsv', payout=500, randomsample=0, min
   # estimate
   estimator = VariableEstimator(ps=probs, Ps=payouts, labels=labels)
   print(estimator)
+
+def loaddata(filename, datatype='flightcsv', minprob=0.001, maxprob=0.20):
+  """
+  Load event probability data.
+  """
+  if datatype is 'flightcsv':
+    data = extract_flight_csv(filename, minprob=minprob, maxprob=maxprob)
+    return data
+  else:
+    raise Exception('unknown datatype %s' % datatype)
