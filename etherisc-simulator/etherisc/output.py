9571214fd985601cdfa00ecb572e8758de2ec97d
Jake Brukhman <jbrukh@gmail.com>
2016-12-03 17:16:38 -0500

    Add simulation module.

--- etherisc-simulator/etherisc/output.py
+++ etherisc-simulator/etherisc/output.py
@@ -2,7 +2,7 @@
 # @Author: Jake Brukhman
 # @Date:   2016-11-30 22:01:55
 # @Last Modified by:   Jake Brukhman
-# @Last Modified time: 2016-12-03 16:19:05
+# @Last Modified time: 2016-12-03 16:22:46
 
 from scipy.stats import norm, uniform
 from etherisc.variable import VariableEstimator
@@ -18,11 +18,18 @@ def estimaterandom(n=10, payout=500):
   estimator = VariableEstimator(ps=ps, Ps=Ps)
   print(estimator)
 
+
 def estimatedata(filename, datatype='flightcsv', payout=500, randomsample=0, minprob=0.001, maxprob=0.20):
   """
   Load actual data for model estimation.
   """
-  data = loaddata(filename, datatype='flightcsv')
+  estimator = __estimatedata(filename, datatype=datatype, payout=payout, 
+    randomsample=randomsample, minprob=minprob, maxprob=maxprob)
+  print(estimator)
+
+
+def __estimatedata(filename, datatype='flightcsv', payout=500, randomsample=0, minprob=0.001, maxprob=0.20):
+  data = __loaddata(filename, datatype='flightcsv')
   if randomsample > 0:
     data = data.sample(randomsample)
   
@@ -33,9 +40,10 @@ def estimatedata(filename, datatype='flightcsv', payout=500, randomsample=0, min
 
   # estimate
   estimator = VariableEstimator(ps=probs, Ps=payouts, labels=labels)
-  print(estimator)
+  return estimator
 
-def loaddata(filename, datatype='flightcsv', minprob=0.001, maxprob=0.20):
+
+def __loaddata(filename, datatype='flightcsv', minprob=0.001, maxprob=0.20):
   """
   Load event probability data.
   """
@@ -44,3 +52,7 @@ def loaddata(filename, datatype='flightcsv', minprob=0.001, maxprob=0.20):
     return data
   else:
     raise Exception('unknown datatype %s' % datatype)
+
+def simulate(filename, datatype='flightcsv', payout=500 minprob=0.001, maxprob=0.20):
+  data = 
+  pass
---
+++ etherisc-simulator/etherisc/simulation.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: Jake Brukhman
+# @Date:   2016-12-03 16:24:30
+# @Last Modified by:   Jake Brukhman
+# @Last Modified time: 2016-12-03 16:24:30
