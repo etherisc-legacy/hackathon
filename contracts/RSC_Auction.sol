2281821fe17e8e6407249a8752dad41683a96db3
Christoph Mussenbrock <christoph2806@users.noreply.github.com>
2016-12-14 22:17:33 +0100

    Update RSC_Auction.sol
    Added some documentation.

--- contracts/RSC_Auction.sol
+++ contracts/RSC_Auction.sol
@@ -1,12 +1,18 @@
+/**
+ * 
+ * @title Auction of tokens
+ * @author Christoph Mussenbrock
+ *
+ */
+
 pragma solidity ^0.4.0;
 import "Tokens/AbstractToken.sol";
 
-
-
-/// @title Dutch auction of tokens, inspired by Gnosis Token sale.
-/// @author Christoph Mussenbrock
 contract RSC_Auction {
 
+    /**
+     * Restrict function call to owner.
+     */
     modifier onlyOwner() {
         if (msg.sender != owner) {
             // Only owner is allowed to proceed
@@ -15,12 +21,18 @@ contract RSC_Auction {
         _;
     }
 
+    /**
+     * Restrict function call to sale period.
+     */
     modifier onlyAtSale {
         if (auctionFinished) {
             throw;
         }        
     }
 
+    /**
+     * Restrict function call to after sales period.
+     */
     modifier onlyAfterSale {
         if (!auctionFinished) {
             throw;
@@ -28,49 +40,122 @@ contract RSC_Auction {
         _;
     }
 
+    /**
+     * Log every bid with investor, amount, chargeable
+     */
     event Log_Bid(address indexed investor, uint256 amount, uint256 chargeable);
 
+    /**
+     * The underlying token.
+     */
     Token public RSC_Token;
 
+    /**
+     * For every bid, we record amount (the value sent) and chargeable 
+     * (what part of the amount counts for the token distribution).
+     */
     struct bid {
         uint amount;
         uint chargeable;
     }
 
-    // we calculate a day with 5760 Blocks (15sec Blocktime), and don't care for small deviations
+    /**
+     * We calculate a day with 5760 Blocks (15sec Blocktime), 
+     * and don't care for small deviations
+     */
 
     uint constant public MINIMUM_SALE_BLOCK   = 7 * 5760;
     uint constant public MAXIMUM_SALE_BLOCK   = 28 * 5760;
-    uint constant public FUNDING_GOAL         =   1000000 ether;
-    uint constant public TOTAL_TOKENS         = 100000000; // 100M
-    uint constant public MAX_TOKENS_SOLD      =  90000000; // 90M
 
-    // Degression: 
-    // 
-    // Funds which are invested later count less.
-    // 
-    // For this, we provide a linear degression scheme. 
+    /**
+     * The funding goal - 1M Ether.
+     * @type {uint}
+     */
+    uint constant public FUNDING_GOAL         =   1000000 ether;
+
+    /**
+     * The targeted token number: 100M 
+     * @type {uint}
+     */
+    uint constant public TOTAL_TOKENS         = 100000000; // 100M
+
+    /**
+     * Degression: 
+     * 
+     * Funds which are invested later count less.
+     * This puts a little incentive to invest early.
+     * For this, we provide a linear degression scheme.
+     * Funds at the beginning count full (with factor 1.0),
+     * Funds at the end count with factor 0.3,
+     * in between there is linear degression.
+     * @type {uint}
+     */
 
     uint constant public DEGRESSION           =       300; // =0.3
+
+    /**
+     * The starting block. Anytime between end of January 2017 and March 2017.
+     * @type {uint}
+     */
     uint constant public STARTBLOCK           =   3141592;
 
+    /**
+     * The total amount raised.
+     */
     uint public totalRaised;
+
+    /**
+     * The total amound charged (after degression)
+     */
     uint public totalCharged;
+
+    /**
+     * Token price.
+     */
     uint public tokenPrice;
+
+    /**
+     * True if auction is finished
+     */
     bool public auctionFinished;
 
+    /**
+     * Mapping with individual bids.
+     * @param  {address} Address of investor
+     * @return {bid}     bid struct.
+     */
     mapping (address => bid) public bids;
 
+    /**
+     * Calculate the degression
+     * @param  {uint} _amount       Invested amount
+     * @return {uint} _chargeable   Degressioned amount
+     */
     function calcDegression(uint _amount) constant returns (uint _chargeable) {
         uint average = (2 * totalRaised + _amount) / 2;
-        return (DEGRESSION * average + 1000 * (FUNDING_GOAL - average)) * _amount / FUNDING_GOAL;
+        return 
+            (DEGRESSION 
+              * average + 1000 * (FUNDING_GOAL - average)
+            ) * _amount 
+            / FUNDING_GOAL;
     }
 
-    function calcTokenCount(uint _chargeable) constant returns (uint _tokenCount) {
+    /**
+     * Calculate token count (only after auction has ended)
+     * @param  {uint} _chargeable   Degressioned amount
+     * @return {uint} _tokenCount   Token Count
+     */
+    function calcTokenCount(uint _chargeable) constant 
+        returns (uint _tokenCount) {
         return _chargeable / tokenPrice;
     }
 
-    /// @dev Allows to send a bid to the auction.
+    /**
+     * The bidding function.
+     * Finalizes auction if called after auction has ended 
+     * (because FUNDING_GOAL has been reached or auction ends)
+     * @return {[type]} [description]
+     */
     function bid()
         public
         onlyAtSale
@@ -91,18 +176,25 @@ contract RSC_Auction {
         bids[msg.sender] = newBid;
         totalRaised += amount;
         Log_Bid(msg.sender, amount, newBid.chargeable);
-        if ((totalRaised > FUNDING_GOAL && block.number > STARTBLOCK + MINIMUM_SALE_BLOCK) 
+        if ((totalRaised > FUNDING_GOAL 
+            && block.number > STARTBLOCK + MINIMUM_SALE_BLOCK) 
             || block.number > STARTBLOCK + MAXIMUM_SALE_BLOCK) {
-            finalizeAuction();
+            endAuction();
         }
     }
 
-    function finalizeAuction()
+    /**
+     * Finish the auction.
+     */
+    function endAuction()
         private
     {
         auctionFinished = true;
     }
 
+    /**
+     * After the sale has ended, investors can claim their tokens.
+     */
     function claimTokens()
         public
         onlyAfterSale
@@ -115,6 +207,10 @@ contract RSC_Auction {
         RSC_Token.transfer(msg.sender, tokenCount);
     }
 
+    /**
+     * After token have been claimed, the remaining money
+     * can be claimed.
+     */
     function claimChange() 
         public
         onlyAfterSale {
@@ -127,7 +223,11 @@ contract RSC_Auction {
         } 
     }
 
-    function setup(address _RSC_Token)
+    /**
+     * Set the Token contract.
+     * @param {[type]} address _RSC_Token [description]
+     */
+    function setTokenContract(address _RSC_Token)
         external
         onlyOwner
     {
